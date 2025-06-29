c4716c69ba7d0a10dfd96333896cf49719b802b8
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-06-29 15:59:42 +0000

    feat: update create invitation view to automatially send invite email with creation of invite
    fix: fix str method for election model
    feat: add custom save method for creation of invitation in admin, to automatically send invitation email upon creation of invitation

--- backend/invitations/admin.py
+++ backend/invitations/admin.py
@@ -1,12 +1,25 @@
 """
 """
 from django.contrib import admin
+
 from invitations.models import Invitation
+from invitations.views import InvitationCreateAPIView
 
 
 @admin.register(Invitation)
 class InvitationAdmin(admin.ModelAdmin):
     """
+    Admin config for managing invitations and sending invite email on creation.
     """
     list_display = ('email', 'election_event', 'is_used', 'created_at')
     search_fields = ('email',)
+
+    def save_model(self, request, obj, form, change):
+        """
+        """
+        is_new = obj.pk is None
+        super().save_model(request, obj, form, change)
+        if is_new:
+            view = InvitationCreateAPIView()
+            view.request = request
+            view.send_invite_email(obj)
